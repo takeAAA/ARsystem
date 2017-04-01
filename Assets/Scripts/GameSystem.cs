fdc7f49de5d7acf612a7361a8b2bc732ccd31eb0
takeAAA <take_19840308@yahoo.co.jp>
2017-04-01 09:34:49 +0900

    新規作成

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour {
+
+	public void Select1 () {
+		SceneManager.LoadScene("Scenes/test");
+	}
+	
+	public void Select2 () {
+		SceneManager.LoadScene("Scenes/Vuforia-3-VideoPlayback");
+	}
+
+	public void Select3 () {
+		SceneManager.LoadScene("Scenes/main");
+	}
+}
